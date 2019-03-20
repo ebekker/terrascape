d1dd8650b9fbc46f1abe2655b6a29aa1bfa7f21c
Eugene Bekker <ebekker@users.noreply.github.com>
2019-03-20 19:08:26 -0400

    first working TF resource in C#!!!

--- src/Terrascape.AcmeProvider/AcmeProvider.FileResource.cs
+++ src/Terrascape.AcmeProvider/AcmeProvider.FileResource.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using HC.TFPlugin;
 using HC.TFPlugin.Attributes;
+using HC.TFPlugin.Diagnostics;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Terrascape.AcmeProvider;
 
 namespace Terrascape.AcmeProvider
@@ -13,28 +18,35 @@ namespace Terrascape.AcmeProvider
         IHasApplyResourceChange<FileResource>,
         IHasReadResource<FileResource>
     {
-        public void ValidateConfig(FileResource res)
-        {
-            _log.LogDebug("{method}: ", nameof(PlanChange));
-            _log.LogTrace("->input = {@res}", res);
-
-
-            _log.LogInformation("NOTE: AcmeProvider Validating Res Config...");
-            _log.LogInformation($"  * res = [{Newtonsoft.Json.JsonConvert.SerializeObject(res)}]");
-        }
-
-        public PlanResourceChangeResult<FileResource> PlanChange(
-            PlanResourceChangeInput<FileResource> input)
+        public ValidateResourceTypeConfigResult<FileResource> ValidateConfig(
+            ValidateResourceTypeConfigInput<FileResource> input)
         {
             _log.LogDebug("{method}: ", nameof(PlanChange));
             _log.LogTrace("->input = {@input}", input);
 
-            var result = new PlanResourceChangeResult<FileResource>()
+            var result = new ValidateResourceTypeConfigResult<FileResource>();
+
+            var contentArgs = 0;
+            if (!string.IsNullOrEmpty(input.Config.Content)) ++contentArgs;
+            if (!string.IsNullOrEmpty(input.Config.ContentBase64)) ++contentArgs;
+            if (!string.IsNullOrEmpty(input.Config.ContentPath)) ++contentArgs;
+            if (!string.IsNullOrEmpty(input.Config.ContentUrl)) ++contentArgs;
+            if (contentArgs != 1)
             {
-                PlannedState = input.ProposedNewState,
-                RequiresReplace = ComputeFileResourceRequiresReplace(input.PriorState, input.Config),
-            };
-            _log.LogInformation("PLANNED.");
+                result.Error("exactly one of the content source arguments must be specified"
+                    + $" ({FileResource.ContentSourceArgumentNames})");
+            }
+
+            var csumKey = input.Config.ComputeChecksum;
+            if (!string.IsNullOrEmpty(csumKey)
+                && csumKey != FileResource.MD5ChecksumKey
+                && csumKey != FileResource.SHA1ChecksumKey
+                && csumKey != FileResource.SHA256ChecksumKey
+                && csumKey != FileResource.SourceCodeHashKey)
+            {
+                result.Error("invalid checksum key, allowed keys: "
+                    + $" ({FileResource.AllowedChecksumKeys})");
+            }
 
             _log.LogTrace("<-result = {@result}", result);
             return result;
@@ -46,11 +58,65 @@ namespace Terrascape.AcmeProvider
             _log.LogDebug("{method}: ", nameof(Read));
             _log.LogTrace("->input = {@input}", input);
 
+            _log.LogDebug("Current: " + JsonConvert.SerializeObject(input.CurrentState, Formatting.Indented));
+
             var result = new ReadResourceResult<FileResource>
             {
-                NewState = input.CurrentState,
+                NewState = new FileResource(),
             };
-            _log.LogInformation("resolving NewState as CurrentState");
+
+            ComputeState(input.CurrentState, result.NewState, result);
+
+            _log.LogTrace("<-result = {@result}", result);
+            return result;
+        }
+
+        public PlanResourceChangeResult<FileResource> PlanChange(
+            PlanResourceChangeInput<FileResource> input)
+        {
+            _log.LogDebug("{method}: ", nameof(PlanChange));
+            _log.LogTrace("->input = {@input}", input);
+
+            _log.LogDebug("  Config: " + JsonConvert.SerializeObject(input.Config, Formatting.Indented));
+            _log.LogDebug("   Prior: " + JsonConvert.SerializeObject(input.PriorState, Formatting.Indented));
+            _log.LogDebug("Proposed: " + JsonConvert.SerializeObject(input.ProposedNewState, Formatting.Indented));
+
+            var result = new PlanResourceChangeResult<FileResource>();
+            if (input.ChangeType == ResourceChangeType.Update)
+                result.RequiresReplace = ResolveRequiresReplace(input.PriorState, input.Config);
+
+            result.PlannedState = new FileResource().CopyArgumentsFrom(input.Config);
+
+            var newContent = input.ChangeType == ResourceChangeType.Create
+                || (input.ChangeType == ResourceChangeType.Update
+                    && (input.Config.Path != input.PriorState?.Path
+                        || input.Config.SourceOfContent != input.PriorState?.SourceOfContent));
+
+            if (!newContent)
+            {
+                result.PlannedState.FullPath = input.PriorState.FullPath;
+                result.PlannedState.LastModified = input.PriorState.LastModified;
+            }
+
+            // If a checksum was requested, we indicate that the corresponding
+            // checksum value property will be computed after the apply using null
+            if (input.ChangeType != ResourceChangeType.Delete
+                && !string.IsNullOrEmpty(input.Config.ComputeChecksum))
+            {
+                if (newContent
+                    || input.Config.ComputeChecksum != input.PriorState.ComputeChecksum)
+                {
+                    _log.LogDebug("Reseting Checksum, to be computed ({prior} -> {config})",
+                        input.PriorState.ComputeChecksum,
+                        input.Config.ComputeChecksum);
+                    result.PlannedState.Checksum = null;
+                }
+                else
+                {
+                    _log.LogDebug("Keeping Checksum");
+                    result.PlannedState.Checksum = input.PriorState.Checksum;
+                }
+            }
 
             _log.LogTrace("<-result = {@result}", result);
             return result;
@@ -62,26 +128,114 @@ namespace Terrascape.AcmeProvider
             _log.LogDebug("{method}: ", nameof(ApplyChange));
             _log.LogTrace("->input = {@input}", input);
 
-            var result = new ApplyResourceChangeResult<FileResource>
+            var result = new ApplyResourceChangeResult<FileResource>();
+            if (input.ChangeType != ResourceChangeType.Delete)
+                result.NewState = new FileResource().CopyArgumentsFrom(input.Config);
+
+            var deleteOld = input.PlannedState.Path != input.PriorState.Path;
+            var createNew = input.ChangeType != ResourceChangeType.Delete
+                && (input.PlannedState.Path != input.PriorState.Path
+                || input.PlannedState.SourceOfContent != input.PriorState.SourceOfContent);
+
+            if (deleteOld)
             {
-                NewState = input.Config,
-            };
-            if (input.Config == null)
-                _log.LogInformation("RES DELETED!");
-            else if (string.IsNullOrEmpty(input.Config.Path))
-                _log.LogInformation("RES PATH MISSING!");
-            _log.LogInformation("APPLIED.");
+                // For Updates & Deletes, remove the old path
+                _log.LogInformation("DELETING old path");
+                File.Delete(input.PriorState.Path);
+            }
+            if (createNew)
+            {
+                // For Creates & Updates, create the new path
+                _log.LogInformation("CREATING new path");
+                if (!string.IsNullOrEmpty(input.Config.Content))
+                {
+                    File.WriteAllText(input.Config.Path,
+                        input.Config.Content);
+                }
+                else if (!string.IsNullOrEmpty(input.Config.ContentBase64))
+                {
+                    File.WriteAllBytes(input.Config.Path,
+                        Convert.FromBase64String(input.Config.ContentBase64));
+                }
+                else if (!string.IsNullOrEmpty(input.Config.ContentPath))
+                {
+                    File.Copy(input.Config.ContentPath, input.Config.Path, true);
+                }
+                else if (!string.IsNullOrEmpty(input.Config.ContentUrl))
+                {
+                    using (var wc = new WebClient())
+                    {
+                        wc.DownloadFile(input.Config.ContentUrl, input.Config.Path);
+                    }
+                }
+            }
+
+            ComputeState(input.Config, result.NewState, result);
 
             _log.LogTrace("<-result = {@result}", result);
             return result;
         }
 
-        private IEnumerable<ValuePath> ComputeFileResourceRequiresReplace(
+        private IEnumerable<TFSteps> ResolveRequiresReplace(
             FileResource priorState, FileResource config)
         {
             // Only the path triggers a new resource (logically)
             if (priorState?.Path != config?.Path)
-                yield return new ValuePath().Attribute(nameof(FileResource.Path));
+                yield return new TFSteps().Attribute(nameof(FileResource.Path));
+        }
+
+        private void ComputeState(FileResource state, FileResource newState, IHasDiagnostics diags)
+        {
+            if (string.IsNullOrEmpty(state?.Path))
+                return;
+
+            newState.CopyArgumentsFrom(state);
+            var file = new FileInfo(state.Path);
+
+            if (file.Exists)
+            {
+                newState.FullPath = file.FullName;
+                newState.LastModified = file.LastWriteTime.ToString("r");
+
+                var csumKey = newState.ComputeChecksum;
+                if (!string.IsNullOrEmpty(csumKey))
+                {
+                    System.Security.Cryptography.HashAlgorithm csumAlgor = null;
+                    switch (csumKey)
+                    {
+                        case FileResource.MD5ChecksumKey:
+                            csumAlgor = System.Security.Cryptography.MD5.Create();
+                            break;
+                        case FileResource.SHA1ChecksumKey:
+                            csumAlgor = System.Security.Cryptography.SHA1.Create();
+                            break;
+                        case FileResource.SHA256ChecksumKey:
+                            csumAlgor = System.Security.Cryptography.SHA256.Create();
+                            break;
+                        case FileResource.SourceCodeHashKey:
+                            csumAlgor = System.Security.Cryptography.MD5.Create();
+                            break;
+                        default:
+                            diags.Error("invalid checksum key");
+                            break;
+                    }
+
+                    if (csumAlgor != null)
+                    {
+                        byte[] csum;
+                        using (csumAlgor)
+                        using (var fs = file.OpenRead())
+                        {
+                            csum = csumAlgor.ComputeHash(fs);
+                        }
+
+                        if (csumKey == FileResource.SourceCodeHashKey)
+                            newState.Checksum = Convert.ToBase64String(csum);
+                        else
+                            newState.Checksum = BitConverter.ToString(csum).Replace("-", "");
+                    }
+                }
+            }
         }
     }
 }
--- src/Terrascape.AcmeProvider/FileResource.cs
+++ src/Terrascape.AcmeProvider/FileResource.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using HC.TFPlugin;
 using HC.TFPlugin.Attributes;
 
@@ -6,41 +8,168 @@ namespace Terrascape.AcmeProvider
     [TFResource("acmelo_file", Version = 2L)]
     public class FileResource
     {
-        [TFAttribute("path",
+        public static readonly IEnumerable<string> ContentSourceProperties = new[]
+        {
+            nameof(FileResource.Content),
+            nameof(FileResource.ContentBase64),
+            nameof(FileResource.ContentPath),
+            nameof(FileResource.ContentUrl),
+        };
+        public static readonly string ContentSourceArgumentNames = string.Join(", ",
+            ContentSourceProperties.Select(x => TFArgumentAttribute.Get<FileResource>(x).Name));
+
+        public const string MD5ChecksumKey = "md5";
+        public const string SHA1ChecksumKey = "sha1";
+        public const string SHA256ChecksumKey = "sha256";
+        public const string SourceCodeHashKey = "sc_hash";
+
+        public static readonly IEnumerable<string> AllChecksumKeys = new[]
+        {
+            MD5ChecksumKey,
+            SHA1ChecksumKey,
+            SHA256ChecksumKey,
+            SourceCodeHashKey,
+        };
+
+        public static readonly string AllowedChecksumKeys = string.Join(", ", AllChecksumKeys);
+
+        /// <summary>
+        /// The path to the file resource to be written.
+        /// </summary>
+        [TFArgument("path",
             Required = true,
             ForceNew = true)]
         public string Path { get; set; }
 
-        [TFAttribute("content",
+        /// <summary>
+        /// Literal string value to use as the content, which will
+        /// written to the file resource as UTF-8-encoded text.
+        /// </summary>
+        [TFArgument("content",
+            Optional = true,
+            ForceNew = true,
             ConflictsWith = new[] {
                 nameof(ContentBase64),
-                nameof(SourcePath),
-                nameof(SourceUrl),
+                nameof(ContentPath),
+                nameof(ContentUrl),
             })]
         public string Content { get; set; }
 
-        [TFAttribute("content_base64",
+        /// <summary>
+        /// (Exactly one of the content source arguments must be specified.)
+        /// Base64-encoded data that will be decoded and written as raw bytes
+        /// to the file resource. This allows safely uploading non-UTF8 binary
+        /// data, but is recommended only for small content such as the result
+        /// of the gzipbase64 function with small text strings.
+        /// For larger objects, use <c>content_path</c> or <c>content_url</c>
+        /// to write the content from an external source stream.
+        /// </summary>
+        [TFArgument("content_base64",
+            Optional = true,
             ConflictsWith = new[] {
                 nameof(Content),
-                nameof(SourcePath),
-                nameof(SourceUrl),
+                nameof(ContentPath),
+                nameof(ContentUrl),
             })]
         public string ContentBase64 { get; set; }
 
-        [TFAttribute("source_path",
+        [TFArgument("content_path",
+            Optional = true,
             ConflictsWith = new[] {
                 nameof(Content),
                 nameof(ContentBase64),
-                nameof(SourceUrl),
+                nameof(ContentUrl),
             })]
-        public string SourcePath { get; set; }
+        public string ContentPath { get; set; }
 
-        [TFAttribute("source_url",
+        [TFArgument("content_url",
+            Optional = true,
             ConflictsWith = new[] {
                 nameof(Content),
                 nameof(ContentBase64),
-                nameof(SourcePath),
+                nameof(ContentPath),
             })]
-        public string SourceUrl { get; set; }
+        public string ContentUrl { get; set; }
+
+        [TFArgument("compute_checksum")]
+        public string ComputeChecksum { get; set; }
+
+        [TFComputed("full_path")]
+        public string FullPath { get; set; }
+
+        /// <summary>
+        /// Last modified date of the file in RFC1123 format
+        /// (e.g. Mon, 02 Jan 2006 15:04:05 MST).
+        /// </summary>
+        [TFComputed("last_modified")]
+        public string LastModified { get; set; }
+
+        // [TFComputed("md5_checksum")]
+        // public string MD5Checksum
+        // {
+        //     get => MD5ChecksumKey == ComputeChecksum ? Checksum : string.Empty;
+        //     set
+        //     {
+        //         if (!string.IsNullOrEmpty(value))
+        //             ComputeChecksum = value;
+        //     }
+        // }
+
+        // [TFComputed("sha1_checksum")]
+        // public string SHA1Checksum
+        // {
+        //     get => SHA1ChecksumKey == ComputeChecksum ? Checksum : string.Empty;
+        //     set
+        //     {
+        //         if (!string.IsNullOrEmpty(value))
+        //             ComputeChecksum = value;
+        //     }
+        // }
+
+        // [TFComputed("sha256_checksum")]
+        // public string SHA256Checksum
+        // {
+        //     get => SHA256ChecksumKey == ComputeChecksum ? Checksum : string.Empty;
+        //     set
+        //     {
+        //         if (!string.IsNullOrEmpty(value))
+        //             ComputeChecksum = value;
+        //     }
+        // }
+
+        // /// <summary>
+        // /// Resolves to the base64-encoded SHA256 hash of the file.
+        // /// This is equivalent to ${base64sha256(file("path/to/file.ext"))},
+        // /// and can be used with other resources to trigger an update.
+        // /// </summary>
+        // [TFComputed("source_code_hash")]
+        // public string SourceCodeHash
+        // {
+        //     get => SourceCodeHashKey == ComputeChecksum ? Checksum : string.Empty;
+        //     set
+        //     {
+        //         if (!string.IsNullOrEmpty(value))
+        //             ComputeChecksum = value;
+        //     }
+        // }
+
+        public string SourceOfContent
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(Content))
+                    return nameof(Content) + "=" + Content;
+                if (!string.IsNullOrEmpty(ContentBase64))
+                    return nameof(ContentBase64) + "=" + ContentBase64;
+                if (!string.IsNullOrEmpty(ContentPath))
+                    return nameof(ContentPath) + "=" + ContentPath;
+                if (!string.IsNullOrEmpty(ContentUrl))
+                    return nameof(ContentUrl) + "=" + ContentUrl;
+                return string.Empty;
+            }
+        }
+
+        [TFComputed("checksum")]
+        public string Checksum { get; set; } = string.Empty;
     }
 }
