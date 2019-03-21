46e7516a10c49cc1df2479f343dcd75083dda021
Eugene Bekker <ebekker@users.noreply.github.com>
2019-03-21 14:00:21 -0400

    enhanced and renamed to conform to standard conventions

--- src/Publish-TFPlugin.ps1
+++ src/Publish-TFPlugin.ps1
@@ -1,8 +1,13 @@
 
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName="Default")]
 param(
     [string]$Project=$PWD,
-    [switch]$CopyToPlugins
+    [string]$Configuration="Release",
+
+    [Parameter(ParameterSetName="Copy", Mandatory)]
+    [switch]$CopyToPluginsFolder,
+    [Parameter(ParameterSetName="Copy")]
+    [string]$TargetPluginsFolder=$null
 )
 
 $projectFile = [System.IO.Path]::Combine($PWD, $Project)
@@ -36,21 +41,30 @@ Write-Verbose "Target Framewwork resolved as [$targetFramework]"
 $dotnet = Get-Command dotnet
 Write-Verbose "DotNet CLI resolved as [$($dotnet.Path)][v$($dotnet.Version)]"
 
-& $($dotnet.Path) publish -c Release $projectRoot
+& $($dotnet.Path) publish -c $Configuration $projectRoot
 if (-not $?) {
     Write-Warning "Last command to publish failed, aborting"
     return
 }
 
-if ($CopyToPlugins) {
+if ($CopyToPluginsFolder) {
     $publishDir = "$projectRoot/bin/Release/$targetFramework/publish"
+    $pluginsDir = $TargetPluginsFolder
 
-    #$tfPluginsDir = [System.IO.Path]::GetFullPath("$($env:APPDATA)/terraform.d/plugins/windows_amd64")
-    $tfPluginsDir = [System.IO.Path]::GetFullPath("$PSScriptRoot/tf-plugins")
+    #$pluginsDir = [System.IO.Path]::GetFullPath("$($env:APPDATA)/terraform.d/plugins/windows_amd64")
 
-    if (-not [System.IO.Directory]::Exists($tfPluginsDir)) {
-        mkdir $tfPluginsDir
+    if (-not $pluginsDir) {
+        $pluginsDir = [System.IO.Path]::Combine($projectRoot, "tf-plugins")
     }
 
-    Copy-Item -Recurse -Path "$pubDir/*" -Destination $tfPluginsDir -Force
+    if (-not [System.IO.Directory]::Exists($pluginsDir)) {
+        Write-Verbose "Creating Target Plugins Folder [$pluginsDir]"
+        mkdir $pluginsDir
+    }
+
+    Write-Verbose "Copying published plugin assets..."
+    Write-Verbose "  from [$publishDir]"
+    Write-Verbose "    to [$pluginsDir]"
+
+    Copy-Item -Recurse -Path "$publishDir/*" -Destination $pluginsDir -Force
 }
