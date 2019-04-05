f93b0bf8590db2368ee100f37839c77b19c05516
Eugene Bekker <ebekker@users.noreply.github.com>
2019-04-05 12:48:15 -0400

    allow forcing delete (USE WITH CAUTION!)

--- src/Terrascape.WinLocalProvider/Registry/RegistryKeyResource.cs
+++ src/Terrascape.WinLocalProvider/Registry/RegistryKeyResource.cs
@@ -16,6 +16,19 @@ namespace Terrascape.WinLocalProvider.Registry
             ForceNew = true)]
         public string Path { get; set; }
 
+        /// <summary>
+        /// When this resource is marked for deletion, by default
+        /// we only delete the registry key if (1) we originally
+        /// created the key to begin with, and (2) it has no subkeys
+        /// and no values after all the values that we manage have
+        /// been removed.  By setting this flag to true, we override
+        /// this behavior and always delete the registry key.
+        /// </summary>
+        /// <value></value>
+        [TFArgument("force_on_delete",
+            Optional = true)]
+        public bool? ForceOnDelete { get; set; }
+
         [TFNested("entry")]
         public Dictionary<string, ArgumentRegValue> Entries { get; set; }
     }
