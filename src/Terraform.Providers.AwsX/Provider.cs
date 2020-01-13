2564c5e086bdf321d6dbb1351fa2021dfe59b169
Eugene Bekker <ebekker@users.noreply.github.com>
2020-01-13 06:08:09 -0500

    future home of some AWS extensions

---
+++ src/Terraform.Providers.AwsX/Provider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Terraform.Providers.AwsX
+{
+    public class Provider
+    {
+        ILogger _log;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
